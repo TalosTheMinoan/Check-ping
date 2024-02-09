21ec803c9833b8282677a59ebc5db7cde484ba5c
TalosTheMinoan <88402424+TalosTheMinoan@users.noreply.github.com>
2024-02-10 00:23:40 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,10 +1,6 @@
-local kickThreshold = 200 -- Adjust this threshold as needed
-
-RegisterCommand('ping', function(source, args, rawCommand)
-    local playerPing = GetPlayerPing(source)
-    TriggerClientEvent('chatMessage', source, '^5[Ping]^0 Your current ping is: ^2' .. playerPing)
-
-    if playerPing > kickThreshold then
-        DropPlayer(source, 'Your ping is too high. Please reconnect with a better connection.')
-    end
-end, false)
+RegisterServerEvent("fetchPlayerLatency")
+AddEventHandler("fetchPlayerLatency", function()
+    local playerId = source
+    local latency = GetPlayerPing(playerId)
+    TriggerClientEvent("displayPing", playerId, latency)
+end)
