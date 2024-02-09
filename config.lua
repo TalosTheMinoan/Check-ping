8433c6486f1cbe036c8f7836af2c67fdb4877b44
TalosTheMinoan <88402424+TalosTheMinoan@users.noreply.github.com>
2024-02-10 00:24:03 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+-- Customize the chat message format and color thresholds here
+Config.ChatMessageFormat = "^3Your current ping is: ^2%s^3 ms"
+Config.GoodPingColor = {0, 255, 0} -- Green color
+Config.MediumPingColor = {255, 165, 0} -- Orange color
+Config.HighPingColor = {255, 0, 0} -- Red color
