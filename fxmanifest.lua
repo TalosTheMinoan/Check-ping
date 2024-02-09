3f1dcdbbbfcb6ecbf39f9088aace73b6296acabc
TalosTheMinoan <88402424+TalosTheMinoan@users.noreply.github.com>
2024-02-10 00:23:11 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,15 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Talos'
-description 'Ping Command for FiveM'
+description 'Check Ping Command for FiveM'
 version '1.0.0'
 
-client_script 'client.lua'
-server_script 'server.lua'
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
